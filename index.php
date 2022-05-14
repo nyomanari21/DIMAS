1911cf7cf96fa0903669cea035648263a4fc94d9
SatRamadhani <satriaramadhani1011@gmail.com>
2022-05-14 21:08:45 +0700

    Co-authored-by: Nyoman Ari S <nyomanari21@users.noreply.github.com> Co-authored-by: deva shofa al fathin <devshf@users.noreply.github.com> Co-authored-by: Raihan "VieR" Fitrah <vier15@users.noreply.github.com> Co-authored-by: ridhaaa325 <ridhaaa325@users.noreply.github.com> Co-authored-by: Hagai Brayen S <H4gai@users.noreply.github.com>

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="Style/style.css" rel="stylesheet">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
@@ -17,15 +17,15 @@
 
 <body>
     <section>
-        <nav class="navbar bg-light fixed-top">
-            <div class="container-fluid header-dimas">
+        <nav class="navbar header-dimas fixed-top">
+            <div class="container-fluid">
                 <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
                     data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <a class="navbar-brand" href="login.html">
+                <a class="navbar-brand me-2 text-white" href="login.html">
                     Logout
-                    <img class="ms-2" src="https://img.icons8.com/fluency-systems-filled/452/exit.png" alt="" width="23"
+                    <img class="me-2" src="https://img.icons8.com/fluency-systems-filled/452/exit.png" alt="" width="23"
                         height="28">
                 </a>
 
@@ -36,34 +36,31 @@
                         <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Masjid Darussalam</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
-                    <div class="offcanvas-body" style="background-color: #79BB45;">
+                    <div class="offcanvas-body bg-primary">
                         <ul class="navbar-nav justify-content-end flex-grow-1 pe-2">
                             <li class="nav-item">
-                                <a class="nav-link" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
                                 <a class="nav-link" href="#"><img src="Assets/rhodes island black.png" alt="" width="26"
-                                        height="24" class="d-inline-block align-text-left me-2"> Home</a>
+                                        height="24" class="d-inline-block align-text-left me-3">Home</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#"><img src="Assets/account.png" alt="" width="26" height="24"
-                                        class="d-inline-block align-text-left me-2"> Tentang Kami</a>
+                                        class="d-inline-block align-text-left me-3">Tentang Kami</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#"><img src="Assets/setting.png" alt="" width="26" height="24"
-                                        class="d-inline-block align-text-left me-2"> Pengaturan Akun</a>
+                                        class="d-inline-block align-text-left me-3">Pengaturan Akun</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#"><img src="Assets/cloud_done.png" alt="" width="26"
-                                        height="24" class="d-inline-block align-text-left me-2"> Database</a>
+                                        height="24" class="d-inline-block align-text-left me-3">Database</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#"><img src="Assets/input.png" alt="" width="20" height="22"
-                                        class="d-inline-block align-text-left me-2"> Input Barang</a>
+                                <a class="nav-link" href="#"><img src="Assets/input.png" alt="" width="26" height="24"
+                                        class="d-inline-block align-text-left me-3">Input Barang</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#"><img src="Assets/restore.png" alt="" width="26" height="24"
-                                        class="d-inline-block align-text-left me-2"> Pemeliharaan</a>
+                                        class="d-inline-block align-text-left me-3">Pemeliharaan</a>
                             </li>
                         </ul>
                     </div>
@@ -71,6 +68,10 @@
             </div>
         </nav>
     </section>
+
+    <h1 class = "text-danger">Sayang, mungkin diriku~</h1>
+    <h1>Sayang, mungkin diriku~</h1>
+    <h1 class = "text-danger">Sayang, mungkin diriku~</h1>
 </body>
 
 </html>
