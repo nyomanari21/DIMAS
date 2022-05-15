ec094426f02974bb52f1af6669cff505deb167fc
SatRamadhani <satriaramadhani1011@gmail.com>
2022-05-15 19:48:49 +0700

    Co-authored-by: ridhaaa325 <ridhaaa325@users.noreply.github.com> Co-authored-by: Nyoman Ari S <nyomanari21@users.noreply.github.com> Co-authored-by: deva shofa al fathin <devshf@users.noreply.github.com> Co-authored-by: Raihan "VieR" Fitrah <vier15@users.noreply.github.com>

--- data_inventaris.php
+++ data_inventaris.php
@@ -26,12 +26,6 @@
     </div>
 
     <section>
-        <div class="input-group rounded">
-            <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-            <span class="input-group-text border-0" id="search-addon">
-                <i class="fas fa-search"></i>
-            </span>
-        </div>
         <div class="container p-5">
             <!-- Isi tabel -->
             <table class="table">
