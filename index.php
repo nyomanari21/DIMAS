d334f04b1eb7099555130cfa743dc24ea5cb3345
SatRamadhani <satriaramadhani1011@gmail.com>
2022-05-15 19:45:43 +0700

    Co-authored-by: ridhaaa325 <ridhaaa325@users.noreply.github.com> Co-authored-by: Nyoman Ari S <nyomanari21@users.noreply.github.com> Co-authored-by: deva shofa al fathin <devshf@users.noreply.github.com>

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link href="Style/style.css" rel="stylesheet">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
@@ -23,11 +23,11 @@
             <img src="Assets/logo_darussalam_nav.png" alt="" class="m-10">
             <p>
                 DIMAS</br>
-                Darussalam  Invetory Management System
+                Darussalam Invetory Management System
             </p>
         </div>
     </section>
-    
+
 </body>
 
 </html>
