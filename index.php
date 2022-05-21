d0410a61a87c8f740b56a8c986f7c72d69d92d50
SatRamadhani <satriaramadhani1011@gmail.com>
2022-05-21 14:08:48 +0700

    Co-authored-by: Nyoman Ari S <nyomanari21@users.noreply.github.com> Co-authored-by: Raihan "VieR" Fitrah <vier15@users.noreply.github.com> Co-authored-by: ridhaaa325 <ridhaaa325@users.noreply.github.com>

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 session_start(); // Start session.
 include("config.php");
 
+if(!isset($_SESSION['identifier']))
+{
+    header("location:login.php");
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -12,10 +17,12 @@ include("config.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link href="Style/style.css" rel="stylesheet">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
     </script>
     <title>Home</title>
 </head>
--- log_barang.php
+++ log_barang.php
@@ -1,3 +1,15 @@
+<?php
+
+session_start(); // Start session.
+include("config.php");
+
+if(!isset($_SESSION['identifier']))
+{
+    header("location:login.php");
+}
+
+?>
+
 <!DOCTYPE html>
 
 <html>
