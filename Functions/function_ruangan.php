b758da69bb28cc34619830e844343da639b1f79c
SatRamadhani <satriaramadhani1011@gmail.com>
2022-05-21 21:09:17 +0700

    Co-authored-by: Nyoman Ari S <nyomanari21@users.noreply.github.com> Co-authored-by: ridhaaa325 <ridhaaa325@users.noreply.github.com> Co-authored-by: Raihan "VieR" Fitrah <vier15@users.noreply.github.com>

--- Functions/function_ruangan.php
+++ Functions/function_ruangan.php
@@ -20,4 +20,36 @@ function show_ruangan()
     return $data;
 }
 
+// fungsi menambahkan data ruangan
+function add_ruangan($data)
+{
+    global $koneksi;
+
+    // menyimpan data dari form
+    $kode = $data['kode_ruangan'];
+    $nama = $data['nama_ruangan'];
+
+    // query sql
+    $sql = "INSERT INTO ruangan VALUES($kode, $nama)";
+
+    // hasil query
+    $result = mysqli_query($koneksi, $sql);
+
+    return mysqli_affected_rows($koneksi);
+}
+
+// fungsi menghapus data ruangan
+function delete_ruangan($kode)
+{
+    global $koneksi;
+
+    // query sql
+    $sql = "DELETE FROM ruangan WHERE kd_ruangan = '$kode'";
+
+    // hasil query
+    $result = mysqli_query($koneksi, $sql);
+
+    return mysqli_affected_rows($koneksi);
+}
+
 ?>
