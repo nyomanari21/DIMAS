739250a3ea4d7610bad05fe3e4f7e5b45e9e675f
SatRamadhani <satriaramadhani1011@gmail.com>
2022-05-22 17:43:42 +0700

    Co-authored-by: Nyoman Ari S <nyomanari21@users.noreply.github.com> Co-authored-by: ridhaaa325 <ridhaaa325@users.noreply.github.com> Co-authored-by: Raihan "VieR" Fitrah <vier15@users.noreply.github.com> Co-authored-by: diendasls <diendasls@users.noreply.github.com>

--- Functions/function_log_barang.php
+++ Functions/function_log_barang.php
@@ -9,7 +9,7 @@ function show_log_barang()
 
     // query sql
     $sql = "SELECT log_barang.username_pengurus, log_barang.kd_barang, barang.nama_barang, log_barang.tanggal_input
-            FROM log_barang JOIN barang ON log_barang.kd_barang = barang.kd_barang";
+    FROM log_barang JOIN barang ON log_barang.kd_barang = barang.kd_barang";
 
     // hasil query
     $result = mysqli_query($koneksi, $sql);
@@ -19,6 +19,17 @@ function show_log_barang()
         $data[] = $row;
     }
     return $data;
+}
+
+function add_log($data)
+{
+    global $koneksi;
+
+    $kode = $data['kode_sumber'];
+    $nama = $data['nama_sumber'];
+
+
+
 }
 
 ?>
